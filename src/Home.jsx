78ccda952cfcf8fb8b99ec3bcb8acbe8214c5918
bless5190 <giovani.marth@gmail.com>
2025-05-02 12:49:47 -0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -63,7 +63,19 @@ export default function Home() {
   }, []);
 
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }} className="bg-[url('/bg-office.jpg')] bg-cover bg-center text-white min-h-screen font-sans bg-fixed">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="relative text-white min-h-screen font-sans"
+      style={{
+        backgroundImage: "url('/bg-office.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundAttachment: 'fixed'
+      }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-70 z-0"></div>>
       {/* Header */}
       <header className="flex justify-between items-center p-6 border-b border-gray-800 shadow-md">
         <img src="/logo_verde_transparente.png" alt="CAST Logo" className="h-20 opacity-80" />
@@ -227,4 +239,3 @@ export default function Home() {
     </motion.div>
   );
 }
-
