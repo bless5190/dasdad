d21b85710085f3d7bf191015157032c9e491bcd8
bless5190 <giovani.marth@gmail.com>
2025-05-04 17:13:34 -0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -21,7 +21,7 @@ const CryptoPrice = ({ id, label }) => {
   return (
    <div className="flex flex-col sm:flex-row sm:justify-between items-center bg-gray-800 rounded p-3 shadow w-full gap-2 sm:gap-0">
       <span className="text-green-400 font-semibold">{label}</span>
-      <span className="text-white">{price ? `R$ ${price.toFixed(2)}` : '...'}</span>
+     <span className="text-white">{price ? `R$ ${price.toFixed(3)}` : '...'}</span>
     </div>
   );
 };
